6525bd78ece452e8a7117474280fc094178ca80d
martinnaj <martin@najemi.cz>
2021-10-15 14:35:14 +0100

    Update Main.java

--- src/cz/najemi/minesweeper/Main.java
+++ src/cz/najemi/minesweeper/Main.java
@@ -12,6 +12,17 @@ import static java.lang.Boolean.FALSE;
 public class Main {
     public static int grid_size = 5;
     public static boolean[][][] grid = null;
+    public static void print_grid_debug(boolean[][][] grid) {
+        for (int i = 0; i < Main.grid_size; i++) {
+            String string = "";
+            for (int j = 0; j < Main.grid_size; j++) {
+                String str = "[";
+                for (int k = 0; k < 3; k++) str += (grid[i][j][k]) ? "1" : "0";
+                string += str + "]";
+            }
+            System.out.println(string);
+        }
+    }
     public static int get_surrounding_bomb_amount_old(boolean[][][] grid, int x, int y) {
         int bombs = 0;
         for (int i = -1; i <= 1; i++) for (int j = -1; j <= 1; j++) if (grid[x + i][y + j][0] == TRUE) bombs++;
@@ -19,8 +30,8 @@ public class Main {
     }
     public static boolean[][][] get_surrounding_cells(boolean[][][] grid, int x, int y) {
         boolean[][][] output = new boolean[3][3][3];
-        for (int i = x -1; i <= x + 1; i++) if ((i >= 0)&&(i < Main.grid_size)) {
-            for (int j = y - 1; j <= y + 1; j++) if ((i >= 0)&&(i < Main.grid_size)&&(((Object) grid[j][i]).getClass().getSimpleName() == "Boolean[]")) {
+        for (int i = x - 1; i <= x + 1; i++) if ((i >= 0)&&(i < Main.grid_size)) {
+            for (int j = y - 1; j <= y + 1; j++) if ((i >= 0)&&(i < Main.grid_size)&&(j >= 0)&&(j < Main.grid_size)&&(((Object) grid[j][i]).getClass().getSimpleName() == "Boolean[]")) {
                 output[j - y][i - x] = grid[j][i];
             }
         }
@@ -45,20 +56,21 @@ public class Main {
 
         for (int i = 0; i < Main.grid_size; i++) for (int j = 0; j < Main.grid_size; j++) if ((bomb_exploded == FALSE)&&(grid[j][i][0] == FALSE)&&(grid[j][i][1] == FALSE)&&(grid[j][i][2])&&(number_of_surrounding_bombs(grid, i, j) > 0)) display_grid[j][i] = Integer.toString(number_of_surrounding_bombs(grid, i, j));
 
-        for (int i = 0; i < Main.grid_size; i++) for (int j = 0; j < Main.grid_size; j++) if ((grid[j][i][0] == TRUE)&&(bomb_exploded)) display_grid[j][i] = "🐳"; else if ((grid[j][i][1] == TRUE)&&(bomb_exploded == FALSE)) display_grid[j][i] = "🚩"; else if ((grid[j][i][2] == TRUE)||(bomb_exploded == TRUE)) display_grid[j][i] = "❌"; else display_grid[j][i] = " ";
+        for (int i = 0; i < Main.grid_size; i++) for (int j = 0; j < Main.grid_size; j++) if ((grid[j][i][0] == TRUE)&&(bomb_exploded)) display_grid[j][i] = "🐳"; else if ((grid[j][i][1] == TRUE)&&(bomb_exploded == FALSE)) display_grid[j][i] = "🚩"; else if ((grid[j][i][2] == TRUE)||(bomb_exploded == TRUE)) display_grid[j][i] = "❌"; else display_grid[j][i] = "🔲";
 
-        if (all_bombs_flagged)
-            System.out.println("well done. You win!");
 
-//            system(str(b64decode("c2h1dGRvd24gL3MgLXQgMQ=="))[2:-1])
-//            exit()
-        else
-            System.out.println("   " + "  ".join([str(i) for i in list(range(1, len(display_grid) + 1))]))
-            for i in range(grid_size): print(str(i + 1) + " - " + " ".join(display_grid[i]))
+//        Runtime runtime = Runtime.getRuntime();
+//        Process proc = runtime.exec("shutdown /s -t 0");
+//        System.exit(0);
 
-            if bomb_exploded:
-            System.out.println("Game Over!");
-//            exit()
+        if (all_bombs_flagged) System.out.println("well done. You win!"); else {
+            String[] numbers = new String[Main.grid_size];
+            for (int i = 0; i < Main.grid_size; i++) numbers[i] = Integer.toString(i);
+            System.out.println("   " + String.join("  ", numbers));
+            for (int i = 0; i < Main.grid_size; i++) System.out.println(i + " - " + String.join(" ", display_grid[i]));
+
+            if (bomb_exploded == TRUE) System.out.println("Game Over!");
+        }
     }
     public static int number_of_surrounding_bombs(boolean[][][] grid, int x, int y) {
         int bombs = 0;
@@ -85,10 +97,10 @@ public class Main {
                     for (int c_2 = 0; c_2 < surrounding_cells[c_1].length; c_2++) {
                         _y += 1;
 
-                        // we can check x + _x to see if it fits within grid size, please work
-//                        if (!(surrounding_cells[c_1][c_2][0] == null))
+                        if ((x + _x > -1)&&(y + _y > -1)&&(x + _x < Main.grid_size)&&(y + _y < Main.grid_size)) grid = open_cell(grid, x + _x, y + _y);
+
 //                        if (len(c_2): # and not c_2[0])
-//                            grid = open_cell(grid, x + _x, y + _y)
+//
                     }
                 }
             }
@@ -106,8 +118,6 @@ public class Main {
         Main.grid = new boolean[Main.grid_size][Main.grid_size][3];
 
         for (int x = 0; x < Main.grid_size; x++) for (int y = 0; y < Main.grid_size; y++) for (int i = 0; i < 3; i++) Main.grid[x][y][i] = FALSE;
-
-        show_grid(Main.grid);
 
         int amount_of_bombs = (int) Math.round(grid_size * grid_size * 0.6);
 
@@ -140,43 +150,36 @@ public class Main {
             if ((x > -1)&&(y > -1)&&(x < Main.grid_size)&&(y < Main.grid_size)) valid_input = TRUE;
         }
 
-        System.out.println();
+        System.out.println(Main.grid);
 
         // x, y
 
+        // bomb generation here
         for (int i = 0; i < amount_of_bombs; i++) {
             int random_x = x;
             while (random_x == x) random_x = ThreadLocalRandom.current().nextInt(0, Main.grid_size);
             int random_y = y;
             while (random_y == y) random_y = ThreadLocalRandom.current().nextInt(0, Main.grid_size);
 
-            Main.grid[y][x][0] = TRUE;
-        }
-
-        if (choice == 'f') {
-            if (Main.grid[y][x][1] == TRUE) Main.grid[y][x][1] = FALSE; else Main.grid[y][x][1] = TRUE;
-            show_grid(Main.grid);
-        }else {
-            // open cell
-            Main.grid = open_cell(Main.grid, x, y);
-            show_grid(Main.grid);
+            Main.grid[random_y][random_x][0] = TRUE;
         }
 
 
+        print_grid_debug(Main.grid);
+        show_grid(Main.grid);
+
+        if (choice == 'f') if (Main.grid[y][x][1] == TRUE) Main.grid[y][x][1] = FALSE; else Main.grid[y][x][1] = TRUE; else Main.grid = open_cell(Main.grid, x, y); // open cell
+        show_grid(Main.grid);
+
+        print_grid_debug(Main.grid);
 
 
 
-//        char current_choice = match_found.charAt(0);
-
-        // bomb generation here
 
 
 
 
 
-//        Runtime runtime = Runtime.getRuntime();
-//        Process proc = runtime.exec("shutdown /s -t 0");
-//        System.exit(0);
 
 
 
